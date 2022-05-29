91007521b0814ca2df3bcd922445333fd3782e42
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-05-29 12:33:07 -1000

    added search form

--- app.js
+++ app.js
@@ -11,13 +11,22 @@ app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routes
-app.get('/', (req, res) => {
-    // set the url of the gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // render the hello-gif view, passing the gifUrl into the view to be displayed
-    res.render('handle-gif', { gifUrl })
-});
 
+// root route
+app.get('/', (req, res) => {
+  console.log(req.query) // => "{ term: hey" }[/bold]
+  res.render('home')
+})
+
+// root route TEST
+// app.get('/', (req, res) => {
+//     // set the url of the gif
+//     const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+//     // render the hello-gif view, passing the gifUrl into the view to be displayed
+//     res.render('handle-gif', { gifUrl })
+// });
+
+// greetings route
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
   const name = req.params.name;
